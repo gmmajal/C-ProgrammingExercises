b16385631297709311e8bf3b29199da700f4b697
Ghulam Mustafa Majal <gmajal@jacobs-alumni.de>
2022-10-15 23:51:52 +0800

    Memory properly freed in pointerOperations.cpp

--- Ch4/pointerOperations.cpp
+++ Ch4/pointerOperations.cpp
@@ -3,11 +3,23 @@
 int main(int argc, char* argv[]){
     int i=5;
     int* p_j;
+
     p_j=&i;
+    std::cout<<"i="<<i<<"\n";
     std::cout<<"p_j="<<*p_j<<"\n";
     *p_j *=5;
+    std::cout<<"i="<<i<<"\n";
     std::cout<<"p_j="<<*p_j<<"\n";
-    int *p_k = new int;
-    p_k=&i;
+    int* p_k = new int;
+    *p_k=i;
+    
+    std::cout<<"i="<<i<<"\n";
     std::cout<<"p_k="<<*p_k<<"\n";
+    
+
+    *p_j=0;
+    std::cout<<"i="<<i<<"\n";
+    std::cout<<"p_j="<<*p_j<<"\n";
+    std::cout<<"p_k="<<*p_k<<"\n";
+    delete p_k;
 }
